165ed2beb816059925b0c3140a46c1b24cc2177d
jenileej <jenilee@painscansystem.com>
2021-06-19 19:49:39 -0600

    apps, urls and templates

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     # first segment of url to be
     path('', include('pages.urls')),
+    path('listings/', include('listings.urls')),
     path('admin/', admin.site.urls),
 ]
